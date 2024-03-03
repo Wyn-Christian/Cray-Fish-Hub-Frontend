0ad3214cc48d3c6203d27576cdb083e9a101ceb5
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-03 11:59:16 +0800

    Add resources to UserSettingsPage

--- src/app/(root)/(client)/user/page.jsx
+++ src/app/(root)/(client)/user/page.jsx
@@ -1,12 +1,20 @@
 import { getCurrentUser } from "@/actions/admin/account";
+import { getAllResourcesByUser } from "@/actions/admin/resources";
 import { getAllThreadsByUser } from "@/actions/users/forums";
 import UserDetails from "@/ui/client/profile/UserDetails";
 
 const UserSettingsPage = async () => {
   const user = await getCurrentUser();
   const threads = await getAllThreadsByUser(user._id);
+  const resources = await getAllResourcesByUser(user._id);
 
-  return <UserDetails user={user} threads={threads.data} />;
+  return (
+    <UserDetails
+      user={user}
+      threads={threads.data}
+      resources={resources.data}
+    />
+  );
 };
 
 export default UserSettingsPage;
