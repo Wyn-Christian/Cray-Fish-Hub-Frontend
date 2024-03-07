205da47183383e7cb57409b957f7b06aaf625291
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-07 19:35:54 +0800

    Add profile picture upload functionality and update user details form

--- src/components/user/PersonalDetailsForm.jsx
+++ src/components/user/PersonalDetailsForm.jsx
@@ -1,73 +1,140 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 import { redirect } from "next/navigation";
 import { enqueueSnackbar } from "notistack";
 
-import { Box, Stack, TextField } from "@mui/material";
+import { Box, Card, CardHeader, Stack, TextField } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 import Section from "../Section";
 import SubmitBtn from "../SubmitBtn";
 
 import { updateUser } from "@/actions/users/account";
+import { isEqual } from "@/utils/tool";
+import UploadProfilePic from "../UploadProfilePic";
+import { uploadImage } from "@/utils/upload";
 
-const PersonalDetailsForm = ({ _id, name, username, email }) => {
-  const [state, formAction] = useFormState(updateUser, { id: _id });
+const PersonalDetailsForm = ({ user }) => {
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState(user);
+  const [profilePic, setProfilePic] = useState(null);
+
+  const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    if (state?.status === "fail") {
-      enqueueSnackbar(state?.message, { variant: "error" });
+    setDisabled(isEqual(formData, user) && profilePic === null);
+  }, [formData, profilePic]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  async function formAction() {
+    const updated_user = {
+      ...formData,
+    };
+
+    if (profilePic) {
+      let profilePath = await uploadImage(profilePic);
+      updated_user.profilePath = profilePath;
     }
-    if (state?.status === "success") {
-      enqueueSnackbar("Update user account successfully!", {
-        variant: "success",
-      });
-      redirect(`/profile/${state.data[0]._id}`);
+
+    let result = await updateUser(updated_user);
+
+    if (result?.status === "fail") {
+      enqueueSnackbar(result?.message, { variant: "error" });
+      setError(result?.message);
+      return;
     }
-  }, [state]);
+    if (result.status === "success") {
+      enqueueSnackbar("User update successfully!", { variant: "success" });
+    }
+  }
 
   return (
-    <Section title="Personal Details" subtitle="Here are your details...">
-      <Stack gap={2} component="form" action={formAction}>
-        <input hidden name="id" defaultValue={_id} />
-        <TextField
-          name="name"
-          label="Full Name"
-          defaultValue={name}
-          fullWidth
-        />
+    <>
+      <Grid xs={12} md={4}>
+        <Card
+          elevation={0}
+          sx={{
+            boxShadow:
+              "#919eab33 0px 0px 2px 0px, #919eab33 0px 12px 24px -4px",
+            borderRadius: 4,
+            zIndex: 0,
+            p: "10px 24px 40px",
+          }}
+        >
+          <CardHeader title="Profile Pic" />
 
-        <TextField
-          name="username"
-          label="Username"
-          defaultValue={username}
-          fullWidth
-          error={state?.message?.includes("Username")}
-          helperText={
-            state?.message?.includes("Username")
-              ? state?.message
-              : "Enter your username"
-          }
-        />
+          <Box mb={5}>
+            <UploadProfilePic
+              photo={profilePic}
+              setPhoto={setProfilePic}
+              defaultPhoto={user?.profilePath}
+            />
+          </Box>
+        </Card>
+      </Grid>
 
-        <TextField
-          name="email"
-          label="Email"
-          defaultValue={email}
-          fullWidth
-          error={state?.message?.includes("Email")}
-          helperText={
-            state?.message?.includes("Email")
-              ? state?.message
-              : "Enter your email"
-          }
-        />
+      <Grid xs={12} md={8}>
+        <Card
+          elevation={0}
+          sx={{
+            boxShadow:
+              "#919eab33 0px 0px 2px 0px, #919eab33 0px 12px 24px -4px",
+            borderRadius: 4,
+            zIndex: 0,
+            px: 3,
+            pb: 3,
+          }}
+        >
+          <CardHeader title="User Details" />
+          <Stack gap={2} component="form" action={formAction}>
+            <input hidden name="id" defaultValue={user._id} />
+            <TextField
+              name="name"
+              label="Full Name"
+              value={formData.name}
+              onChange={handleInputChange}
+              fullWidth
+            />
 
-        <Box alignSelf="flex-end" width={{ xs: "100%", sm: "auto" }}>
-          <SubmitBtn title="Save Changes" />
-        </Box>
-      </Stack>
-    </Section>
+            <TextField
+              name="username"
+              label="Username"
+              value={formData.username}
+              onChange={handleInputChange}
+              fullWidth
+              error={error?.includes("Username")}
+              helperText={
+                error?.includes("Username") ? error : "Enter your username"
+              }
+            />
+
+            <TextField
+              name="email"
+              label="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+              fullWidth
+              error={error?.includes("Email")}
+              helperText={error?.includes("Email") ? error : "Enter your email"}
+            />
+
+            <Box alignSelf="flex-end" width={{ xs: "100%", sm: "auto" }}>
+              <SubmitBtn title="Save Changes" disabled={disabled} />
+            </Box>
+          </Stack>
+          {/* </Section> */}
+        </Card>
+      </Grid>
+    </>
   );
 };
 
