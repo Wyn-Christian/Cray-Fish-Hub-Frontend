ee314859a8c99424115ae5ae0cf5c4a679927f6d
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-07 23:05:45 +0800

    Refactor createThread function to simplify code

--- src/actions/users/forums.jsx
+++ src/actions/users/forums.jsx
@@ -46,17 +46,15 @@ export const getThreadDetails = async (id) => {
   return result;
 };
 
-export const createThread = async (currentState, formData) => {
-  const new_thread = Object.fromEntries(formData);
-
-  new_thread.author = cookies().get("currentUser").value;
+export const createThread = async (data) => {
+  data.author = cookies().get("currentUser").value;
 
   const response = await fetch(`${process.env.SERVER_URL}/forumthreads`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(new_thread),
+    body: JSON.stringify(data),
     next: { tags: ["threads"] },
   });
 
@@ -64,6 +62,7 @@ export const createThread = async (currentState, formData) => {
 
   if (result.status == "success") {
     revalidateTag("threads");
-    redirect(`/forums/thread/${result.data[0]._id}`);
   }
+
+  return result;
 };
