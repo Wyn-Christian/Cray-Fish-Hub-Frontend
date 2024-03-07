0a561a31024dfdfa321bb4068dc5d41b05ca9f41
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-07 23:06:50 +0800

    Refactor ThreadsCreateForm component and add image upload functionality

--- src/ui/dashboard/threads/ThreadsCreateForm.jsx
+++ src/ui/dashboard/threads/ThreadsCreateForm.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { useFormState } from "react-dom";
 
 import {
   FormControl,
@@ -15,34 +14,86 @@ import Grid from "@mui/material/Unstable_Grid2";
 import Section from "@/components/Section";
 import { createThread } from "@/actions/admin/forums";
 import SubmitBtn from "@/components/SubmitBtn";
+import { uploadDocuments } from "@/utils/upload";
+import UploadThreadImages from "@/components/UploadThreadImages";
+import { enqueueSnackbar } from "notistack";
+import { redirect } from "next/navigation";
 
 const ThreadsCreateForm = () => {
-  const [state, formAction] = useFormState(createThread, {});
+  const [images, setImages] = useState([]);
 
-  const [category, setCategory] = useState("General");
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "",
+  });
 
-  const handleChange = (event) => {
-    setCategory(event.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
+
+  async function formAction() {
+    const uploaded_images = await uploadDocuments(images);
+
+    if (!uploaded_images) {
+      enqueueSnackbar("Image upload failed, try again...", {
+        variant: "error",
+      });
+      return;
+    }
+
+    const new_thread = {
+      ...formData,
+      images: uploaded_images,
+    };
+
+    let result = await createThread(new_thread);
+
+    if (result?.status === "success") {
+      enqueueSnackbar("Create Thread successfully!", {
+        variant: "success",
+      });
+      redirect(`/admin/forums/details/${result.data[0]._id}`);
+    } else {
+      console.log(result);
+      enqueueSnackbar("Create Thread failed!", {
+        variant: "error",
+      });
+    }
+  }
+
   return (
     <form action={formAction}>
       <Grid container>
         <Section title="Details" subtitle="Title, Category...">
           <Stack gap={1}>
-            <TextField name="title" label="Title" fullWidth />
-            <FormControl fullWidth>
+            <TextField
+              name="title"
+              label="Title"
+              value={formData.title}
+              onChange={handleInputChange}
+              fullWidth
+              required
+            />
+            <FormControl fullWidth required>
               <InputLabel>Category</InputLabel>
               <Select
                 name="category"
-                value={category}
                 label="Category"
-                onChange={handleChange}
+                value={formData.category}
+                onChange={handleInputChange}
               >
                 <MenuItem value={"General"}>General</MenuItem>
                 <MenuItem value={"Q&A"}>Q&A</MenuItem>
                 <MenuItem value={"Case Study"}>Case Study</MenuItem>
               </Select>
             </FormControl>
+
+            <UploadThreadImages setAllImages={setImages} />
           </Stack>
         </Section>
         <Grid md={4} />
