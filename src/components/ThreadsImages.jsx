2385e33662b989c360e40a9126d30272fd9de543
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-07 23:06:27 +0800

    Add ThreadsImages component

---
+++ src/components/ThreadsImages.jsx
@@ -0,0 +1,24 @@
+import { Box, ImageList } from "@mui/material";
+import React from "react";
+import PreviewImage from "./PreviewImage";
+
+const ThreadsImages = ({ images }) => (
+  <Box
+    sx={{
+      m: "auto",
+      width: "100%",
+      maxWidth: 700,
+      maxHeight: 450,
+      overflowY: "auto",
+      mt: 2,
+    }}
+  >
+    <ImageList variant="masonry" cols={2} gap={8}>
+      {images.map((image, i) => (
+        <PreviewImage key={i} src={image.path} alt={`Preview ${i}`} />
+      ))}
+    </ImageList>
+  </Box>
+);
+
+export default ThreadsImages;
