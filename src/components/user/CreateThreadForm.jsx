8818da37a8a9f30339d02b1359d54f7f9d6e33b1
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-07 23:06:10 +0800

    Add image upload functionality and handle form submission in CreateThreadForm

--- src/components/user/CreateThreadForm.jsx
+++ src/components/user/CreateThreadForm.jsx
@@ -17,17 +17,60 @@ import Section from "../Section";
 import SubmitBtn from "../SubmitBtn";
 
 import { createThread } from "@/actions/users/forums";
+import { enqueueSnackbar } from "notistack";
+import UploadThreadImages from "../UploadThreadImages";
+import { uploadDocuments } from "@/utils/upload";
+import { redirect } from "next/navigation";
 
 const CreateThreadForm = ({ userType }) => {
   const isUserRegistered = userType === "Registered";
 
-  const [state, formAction] = useFormState(createThread, {});
+  const [images, setImages] = useState([]);
 
-  const [category, setCategory] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "",
+  });
 
-  const handleChange = (event) => {
-    setCategory(event.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
+
+  async function formAction() {
+    const uploaded_images = await uploadDocuments(images);
+
+    if (!uploaded_images) {
+      enqueueSnackbar("Image upload failed, try again...", {
+        variant: "error",
+      });
+      return;
+    }
+
+    const new_thread = {
+      ...formData,
+      images: uploaded_images,
+    };
+
+    let result = await createThread(new_thread);
+
+    if (result?.status === "success") {
+      enqueueSnackbar("Create Thread successfully!", {
+        variant: "success",
+      });
+
+      redirect(`/admin/forums/details/${result.data[0]._id}`);
+    } else {
+      enqueueSnackbar("Create Thread failed!", {
+        variant: "error",
+      });
+    }
+  }
+
   return (
     <Grid container spacing={3}>
       <Section title="Create Thread" subtitle="Create your thread here...">
@@ -35,6 +78,8 @@ const CreateThreadForm = ({ userType }) => {
           <TextField
             name="title"
             label="Title"
+            value={formData.title}
+            onChange={handleInputChange}
             fullWidth
             required
             disabled={!isUserRegistered}
@@ -44,9 +89,9 @@ const CreateThreadForm = ({ userType }) => {
             <InputLabel>Category</InputLabel>
             <Select
               name="category"
-              value={category}
               label="Category"
-              onChange={handleChange}
+              value={formData.category}
+              onChange={handleInputChange}
             >
               <MenuItem value={"General"}>General</MenuItem>
               <MenuItem value={"Q&A"}>Q&A</MenuItem>
@@ -54,7 +99,9 @@ const CreateThreadForm = ({ userType }) => {
             </Select>
           </FormControl>
 
-          <Box alignSelf="flex-end" width={{ xs: "100%", sm: "auto" }}>
+          <UploadThreadImages setAllImages={setImages} />
+
+          <Box alignSelf="flex-end" width="auto">
             <SubmitBtn
               title={
                 isUserRegistered
