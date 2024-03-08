8096386c5effa4c849eaf548c2abe7faab1368ec
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-08 23:28:38 +0800

    Refactor ArticleComment component

--- src/components/ArticleComment.jsx
+++ src/components/ArticleComment.jsx
@@ -1,40 +1,63 @@
-import { Avatar, Box, Divider, Stack, Typography } from "@mui/material";
 import moment from "moment";
 import Link from "next/link";
 
-const ArticleComment = ({ author, content, createdAt }) => (
-  <Stack direction="row" gap={1}>
-    <Box
-      component={Link}
-      href={`/profile/${author._id}`}
-      sx={{ textDecoration: "none" }}
-    >
-      <Avatar
-        alt={author?.name}
-        src={author?.profilePath || "/assets/profile/img-1.png"}
-      />
-    </Box>
-    <Stack flexGrow={1}>
+import { Avatar, Box, Divider, Stack, Typography } from "@mui/material";
+import DeleteBtn from "./DeleteBtn";
+import { deleteArticleComment } from "@/actions/admin/articles";
+
+const ArticleComment = ({
+  _id,
+  author,
+  content,
+  createdAt,
+  isDeletable = false,
+}) => {
+  return (
+    <Stack direction="row" gap={1}>
       <Box
         component={Link}
         href={`/profile/${author._id}`}
-        sx={{
-          textDecoration: "none",
-          color: "inherit",
-          "&:hover": { textDecoration: "underline" },
-        }}
+        sx={{ textDecoration: "none" }}
       >
-        <Typography variant="subtitle2">{author?.name}</Typography>
+        <Avatar
+          alt={author?.name}
+          src={author?.profilePath || "/assets/profile/img-1.png"}
+        />
       </Box>
-      <Typography variant="caption" color="#919eab">
-        {moment(createdAt).fromNow()}
-      </Typography>
-      <Typography variant="body2" my={1}>
-        {content}
-      </Typography>
-      <Divider />
+      <Stack flexGrow={1}>
+        <Stack direction="row" justifyContent="space-between">
+          <Box>
+            <Box
+              component={Link}
+              href={`/profile/${author._id}`}
+              sx={{
+                textDecoration: "none",
+                color: "inherit",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              <Typography variant="subtitle2">{author?.name}</Typography>
+            </Box>
+            <Typography variant="caption" color="#919eab">
+              {moment(createdAt).fromNow()}
+            </Typography>
+          </Box>
+          {isDeletable && (
+            <DeleteBtn
+              title="Delete this comment?"
+              action={deleteArticleComment}
+              id={_id}
+              iconOnly={true}
+            />
+          )}
+        </Stack>
+        <Typography variant="body2" my={1}>
+          {content}
+        </Typography>
+        <Divider />
+      </Stack>
     </Stack>
-  </Stack>
-);
+  );
+};
 
 export default ArticleComment;
