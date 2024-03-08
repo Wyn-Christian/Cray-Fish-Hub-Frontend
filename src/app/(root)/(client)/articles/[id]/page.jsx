874c5634f068e4d33c97966492094ae0431f4d50
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-09 00:21:39 +0800

    Add user information to ArticleDetails component

--- src/app/(root)/(client)/articles/[id]/page.jsx
+++ src/app/(root)/(client)/articles/[id]/page.jsx
@@ -2,15 +2,23 @@ import { Box } from "@mui/material";
 import ArticleDetails from "@/ui/client/articles/ArticleDetails";
 import { getArticleComments, getArticleDetail } from "@/actions/admin/articles";
 import { isUserLogin } from "@/actions/users/account";
+import { getCurrentUser } from "@/actions/admin/account";
 
 const ArticleDetailPage = async ({ params }) => {
   let article = await getArticleDetail(params.id);
   let comments = await getArticleComments(params.id);
+  let user = await getCurrentUser();
+
   let isLogin = isUserLogin();
 
   return (
     <Box mb={30}>
-      <ArticleDetails {...article} comments={comments} isLogin={isLogin} />
+      <ArticleDetails
+        {...article}
+        comments={comments}
+        isLogin={isLogin}
+        user={user}
+      />
     </Box>
   );
 };
