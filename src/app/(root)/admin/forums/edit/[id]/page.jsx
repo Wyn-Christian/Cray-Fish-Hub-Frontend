9ada77560f241f4b20479adf9d047a9c9ccdbb85
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-08 09:02:22 +0800

    Add ThreadsEditForm component to ForumEditPage

--- src/app/(root)/admin/forums/edit/[id]/page.jsx
+++ src/app/(root)/admin/forums/edit/[id]/page.jsx
@@ -3,6 +3,7 @@ import { Container } from "@mui/material";
 import DashboardBreadcrumbs from "@/components/admin/DashboardBreadcrumbs";
 
 import { getThreadDetails } from "@/actions/admin/forums";
+import ThreadsEditForm from "@/ui/dashboard/threads/ThreadsEditForm";
 
 const links = [
   {
@@ -30,6 +31,8 @@ const ForumEditPage = async ({ params }) => {
       }}
     >
       <DashboardBreadcrumbs title="Edit" links={links} />
+
+      <ThreadsEditForm thread={thread} />
     </Container>
   );
 };
