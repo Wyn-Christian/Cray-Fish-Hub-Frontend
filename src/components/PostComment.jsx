2253e662dff115f173109edd55a08573c7c4c85a
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-03-08 23:28:49 +0800

    Add DeleteBtn component to PostComment.jsx

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -9,48 +9,65 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
+import DeleteBtn from "./DeleteBtn";
+import { deletePostComment } from "@/actions/admin/forums";
 
-const PostComment = ({ content, createdAt, author }) => {
+const PostComment = ({
+  _id,
+  content,
+  createdAt,
+  author,
+  isDeletable = false,
+}) => {
   return (
     <Fade in>
       <Paper sx={{ pt: 1, height: "100%" }} elevation={0}>
-        <Stack gap={1} direction="row">
-          <Stack alignItems="center" gap={0.5}>
-            <Avatar
-              alt={author?.name}
-              src={author?.profilePath || "/assets/profile/img-1.png"}
-              sx={{ width: 50, height: 50 }}
-            />
-            <Box height="100%">
-              <Divider orientation="vertical" />
-            </Box>
-          </Stack>
-
-          <Stack>
-            <Stack>
-              <Stack
-                direction={["column", "row"]}
-                alignItems={{ sm: "center" }}
-                gap={[0, 1]}
-              >
-                <Typography variant="subtitle2">{author?.name}</Typography>
-
-                <Box
-                  width={4}
-                  height={4}
-                  borderRadius="50%"
-                  bgcolor="#333"
-                  display={["none", "block"]}
-                />
-
-                <Typography variant="caption" color="#888">
-                  {moment(createdAt).fromNow()}
-                </Typography>
-              </Stack>
+        <Stack direction="row" justifyContent="space-between">
+          <Stack gap={1} direction="row">
+            <Stack alignItems="center" gap={0.5}>
+              <Avatar
+                alt={author?.name}
+                src={author?.profilePath || "/assets/profile/img-1.png"}
+                sx={{ width: 50, height: 50 }}
+              />
+              <Box height="100%">
+                <Divider orientation="vertical" />
+              </Box>
             </Stack>
 
-            <Typography variant="body1">{content}</Typography>
+            <Stack>
+              <Stack>
+                <Stack
+                  direction={["column", "row"]}
+                  alignItems={{ sm: "center" }}
+                  gap={[0, 1]}
+                >
+                  <Typography variant="subtitle2">{author?.name}</Typography>
+
+                  <Box
+                    width={4}
+                    height={4}
+                    borderRadius="50%"
+                    bgcolor="#333"
+                    display={["none", "block"]}
+                  />
+
+                  <Typography variant="caption" color="#888">
+                    {moment(createdAt).fromNow()}
+                  </Typography>
+                </Stack>
+              </Stack>
+
+              <Typography variant="body1">{content}</Typography>
+            </Stack>
           </Stack>
+
+          <DeleteBtn
+            id={_id}
+            title="Delete this post comment?"
+            action={deletePostComment}
+            iconOnly
+          />
         </Stack>
       </Paper>
     </Fade>
